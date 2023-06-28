fa3cfc23c1a486815e2869db96a0bfe3be21697d
Dasha Savostina <moskvichka123@yandex.ru>
2023-06-28 16:17:48 +0300

    14ПР

--- middlewares/validationJoi.js
+++ middlewares/validationJoi.js
@@ -4,11 +4,11 @@ const regular = /(http?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]
 
 module.exports.createUserValidation = celebrate({
   body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().min(4).pattern(regular),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
   }),
 });
 
