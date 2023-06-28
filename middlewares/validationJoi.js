dc5a3a670876fc226f0a45679e1d6dfb244f9f35
Dasha Savostina <moskvichka123@yandex.ru>
2023-06-28 16:39:59 +0300

    14ПР

--- middlewares/validationJoi.js
+++ middlewares/validationJoi.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 
-const regular = /(http?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^/s]{2,}|http?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^/s]{2,}|www\.[a-zA-Z0-9]+\.[^/s]{2,})/;
+const regular = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports.createUserValidation = celebrate({
   body: Joi.object().keys({
